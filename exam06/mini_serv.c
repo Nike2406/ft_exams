8cd053a0562b287a5c2bdf42a6b8ca15f78c2304
Nike2406 <locus2@mail.ru>
2022-10-19 21:53:13 +0300

    5th

--- exam06/mini_serv.c
+++ exam06/mini_serv.c
@@ -5,17 +5,6 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-
-int max_fd;
-int arr_id[5000];
-char *arr_str[5000];
-char *msg;
-fd_set write_set, read_set, actual_set;
-char buff_read[1001];
-char buff_send[100];
-
 int extract_message(char **buf, char **msg)
 {
 	char	*newbuf;
@@ -63,97 +52,43 @@ char *str_join(char *buf, char *add)
 	return (newbuf);
 }
 
-void print(int fd, char *str) {
-	write(fd, str, strlen(str));
-	write(fd, "\n", 1);
-	exit(1);
-}
 
-void send_msg(int fd) {
-	for (int current_fd = 3; current_fd <= max_fd; current_fd++) {
-		if (FD_ISSET(current_fd, &write_set) && current_fd != fd) {
-			send(current_fd, buff_send, strlen(buff_send), 0);
-			if (msg) {
-				send(current_fd, msg, strlen(msg), 0);
-			}
-		}
-	}
-}
-
-int main(int ac, char **av) {
-	int sockfd, connfd;
-	socklen_t len;
+int main() {
+	int sockfd, connfd, len;
 	struct sockaddr_in servaddr, cli;
 
 	// socket create and verification
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1) {
-		print(2, "Wrong args");
+		printf("socket creation failed...\n");
+		exit(0);
 	}
+	else
+		printf("Socket successfully created..\n");
 	bzero(&servaddr, sizeof(servaddr));
 
 	// assign IP, PORT
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
-	servaddr.sin_port = htons(atoi(av[1]));
+	servaddr.sin_port = htons(8081);
 
 	// Binding newly created socket to given IP and verification
 	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) {
-		print(2, "Fatal error");
+		printf("socket bind failed...\n");
+		exit(0);
 	}
+	else
+		printf("Socket successfully binded..\n");
 	if (listen(sockfd, 10) != 0) {
-		print(2, "Fatal error");
+		printf("cannot listen\n");
+		exit(0);
 	}
 	len = sizeof(cli);
-
-	max_fd = sockfd;
-	FD_ZERO(&actual_set);
-	FD_SET(sockfd, &actual_set);
-	int client_id = 0;
-
-	while(1) {
-		read_set = write_set = actual_set;
-		if (select(max_fd + 1, &read_set, &write_set, NULL, NULL) <= 0)
-			continue;
-
-		if (FD_ISSET(sockfd, &read_set)) {
-			connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
-			if (connfd < 0) {
-				print(2, "Fatal error");
-			}
-			arr_id[connfd] = client_id;
-			FD_SET(connfd, &actual_set);
-			max_fd = connfd > max_fd ? connfd : max_fd;
-			sprintf(buff_send, "%d join", arr_id[connfd]);
-			arr_str[connfd] = NULL;
-			send_msg(connfd);
-			continue;
-		}
-
-		for (int fd = 3; fd <= max_fd; fd++) {
-			 if (FD_ISSET(fd, &read_set) && fd != sockfd) {
-				size_t count = recv(fd, buff_read, 1000, 0);
-				if (count <= 0) {
-					FD_CLR(fd, &actual_set);
-					sprintf(buff_send, "%d left", arr_id[fd]);
-					send_msg(fd);
-					close(fd);
-					if (arr_str[fd] != 0)
-						free(arr_str[fd]);
-					break;
-				} else {
-					buff_read[count] = '\0';
-					arr_str[fd] = str_join(arr_str[fd], buff_read);
-					msg = NULL;
-
-					while (extract_message(arr_str[fd], msg)) {
-						sprintf(buff_send, "client: %d", arr_id[fd]);
-						send_msg(fd);
-					}
-					free(msg);
-					msg = NULL;
-				}
-			 }
-		}
-	}
+	connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
+	if (connfd < 0) {
+        printf("server acccept failed...\n");
+        exit(0);
+    }
+    else
+        printf("server acccept the client...\n");
 }
---
+++ exam06/original_c/mini_serv_05.c
@@ -0,0 +1,110 @@
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_clients {
+	int		id;
+	char	msg[1024];
+} t_clients;
+
+t_clients	clients[1024];
+fd_set		read_set, write_set, active_set;
+int 		max_fd = 0, id_next = 0;
+char		buff_read[65536 * 2], buff_write[65536 * 2];
+
+void ftError(char *str) {
+	if (str)
+		write(2, str, strlen(str));
+	else
+		write(2, "Fatal error", strlen("Fatal error"));
+	write(2, "\n", 1);
+	exite(1);
+}
+
+void sendAll(int fd) {
+	for (int i = 0; i <= max_fd; i++) {
+		if (FD_ISSET(i, &write_set) && i != fd) {
+			send(i, buff_write, strlen(buff_write), 0);
+		}
+	}
+}
+
+int main(int ac, char **av) {
+	if (ac != 2) {
+		frError("Wrong number of arguments");
+	}
+
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		ftError(NULL);
+	}
+
+	FD_ZERO(&active_set);
+	bzero(&clients, sizeof(clients));
+	max_fd = sockfd;
+	FD_SET(sockfd, &active_set);
+
+	struct sockaddr_in	servaddr;
+	socklen_t			len;
+	bzero(sockfd, &active_set);
+
+	// assign IP, PORT
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
+	servaddr.sin_port = htons(av[1]);
+
+	// Binding newly created socket to given IP and verification
+	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) {
+		ftError(NULL);
+	}
+	if (listen(sockfd, 10) != 0) {
+		ftError(NULL);
+	}
+
+	while (1) {
+		read_set = write_set = active_set;
+		if (select(max_fd + 1, &read_set, &write_set, NULL, NULL) < 0)
+			continue;
+
+		for (int fd = 0; fd <= max_fd; fd++) {
+			if (FD_ISSET(fd, &read_set) && fd == sockfd) {
+				int connfd = accept(sockfd, (struct sockaddr *)&servaddr, &len);
+				if (connfd < 0) {
+					continue;
+				}
+				max_fd = connfd > max_fd ? connfd : max_fd;
+				clients[connfd].id = id_next++;
+				FD_SET(connfd, &active_set);
+				sprintf(buff_write, "server: client %d just arrived\n", clients[connfd].id);
+				sendAll(connfd);
+				break;
+			}
+			if (FD_ISSET(fd, &read_set) && fd != sockfd) {
+				int res = recv(fd, buff_read, 65536 * 2, 0);
+				if (res <= 0) {
+					sprintf(buff_write, "server: client %d just left\n", clients[fd].id);
+					sendAll(fd);
+					FD_CLR(fd, &active_set);
+					close(fd);
+					break;
+				} else {
+					for (int i = 0, j = strlen(clients[fd].msg); i < res; i++, j++) {
+						if (clients[fd].msg[j] == '\n') {
+							clients[fd].msg[j] = '\0';
+							sprintf(buff_write, "client %d: %s\n", clients[fd].id, clients[fd].msg);
+							sendAll(fd);
+							bzero(&clients[fd].msg, strlen(clients[fd].msg));
+							j = -1;
+						}
+					}
+					break;
+				}
+			}
+		}
+	}
+}
